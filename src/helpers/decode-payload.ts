15f883f1296057d398fe55d917634b02e04ccd51
gentlementlegen <fernand.veyrier@epitech.eu>
2025-08-26 14:29:16 +0200

    chore: properly fetching data from the env

--- src/helpers/decode-payload.ts
+++ src/helpers/decode-payload.ts
@@ -1,5 +1,12 @@
 import { createActionsPlugin } from "@ubiquity-os/plugin-sdk";
+import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 
-createActionsPlugin((context) => {
-  return context.payload;
-}).catch(console.error);
+createActionsPlugin(
+  (context) => {
+    return context.payload;
+  },
+  {
+    logLevel: (process.env.LOG_LEVEL as LogLevel) ?? LOG_LEVEL.INFO,
+    kernelPublicKey: process.env.KERNEL_PUBLIC_KEY,
+  }
+).catch(console.error);
