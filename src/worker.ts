8955abe126fc68c38e696185607ec4f5667a93c7
gentlementlegen <fernand.veyrier@epitech.eu>
2026-04-09 14:45:35 +0900

    fix: update homepage_url manifest

--- src/worker.ts
+++ src/worker.ts
@@ -1,17 +1,30 @@
 import { createPlugin, type Options } from "@ubiquity-os/plugin-sdk";
+import { Manifest, resolveRuntimeManifest } from "@ubiquity-os/plugin-sdk/manifest";
+import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import type { ExecutionContext } from "hono";
+import manifest from "../manifest.json";
+import { plugin } from "./plugin";
+import { Command } from "./types/command";
 import { SupportedEvents } from "./types/context";
 import { Env, envSchema } from "./types/env";
 import { PluginSettings, pluginSettingsSchema } from "./types/plugin-input";
-import manifest from "../manifest.json";
-import { plugin } from "./plugin";
-import { Manifest } from "@ubiquity-os/plugin-sdk/manifest";
-import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
-import { Command } from "./types/command";
+
+function buildRuntimeManifest(request: Request) {
+  const runtimeManifest = resolveRuntimeManifest(manifest as Manifest);
+  return {
+    ...runtimeManifest,
+    homepage_url: new URL(request.url).origin,
+  };
+}
 
 export default {
   async fetch(request: Request, env: Env, executionCtx?: ExecutionContext) {
-    return createPlugin<PluginSettings, Env, Command, SupportedEvents>((context) => plugin(context), manifest as Manifest, {
+    const runtimeManifest = buildRuntimeManifest(request);
+    if (new URL(request.url).pathname === "/manifest.json") {
+      return Response.json(runtimeManifest);
+    }
+
+    return createPlugin<PluginSettings, Env, Command, SupportedEvents>((context) => plugin(context), runtimeManifest, {
       envSchema: envSchema as unknown as Options["envSchema"],
       postCommentOnError: true,
       settingsSchema: pluginSettingsSchema as unknown as Options["settingsSchema"],
