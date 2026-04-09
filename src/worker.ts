41809ade5a9cb5f507a74e9808080fba397eeca0
gentlementlegen <fernand.veyrier@epitech.eu>
2026-04-09 16:38:34 +0900

    fix: add json import attributes for deno

--- src/worker.ts
+++ src/worker.ts
@@ -2,7 +2,7 @@ import { createPlugin, type Options } from "@ubiquity-os/plugin-sdk";
 import { Manifest, resolveRuntimeManifest } from "@ubiquity-os/plugin-sdk/manifest";
 import { LOG_LEVEL, LogLevel } from "@ubiquity-os/ubiquity-os-logger";
 import type { ExecutionContext } from "hono";
-import manifest from "../manifest.json";
+import manifest from "../manifest.json" with { type: "json" };
 import { plugin } from "./plugin";
 import { Command } from "./types/command";
 import { SupportedEvents } from "./types/context";
